08416085c0b57f806fc0c01047afbae7dfd611f7
Realgabbe <gabriel.billskog@elev.ga.ntig.se>
2019-09-09 13:16:08 +0200

    LEKTION 1 MED RIKTIG PROGRAMMERING VA

---
+++ VECKA37/VECKA37/LEKTION1.java
@@ -0,0 +1,33 @@
+package VECKA37;
+
+public class LEKTION1 {
+
+	public static void main(String[] args) {
+		// Ett sätt att skriva kommentarer
+		/* Ett annat sätt att skriva kommentarer
+		 * Det här är mellanraderna
+		 * 
+		 */ 
+		
+		int heltal1 = 25; // Skapar en låda som heter heltal1 och fyller med ett heltal
+		int svar; // deklarera
+		
+		svar= heltal1*heltal1;
+		
+		System.out.println("svar: " + heltal1*heltal1); // + tecknet kallas konkatinera
+		
+		double pi = 3.14;
+		int radie =5;
+		
+		double area = pi*radie*radie;
+		
+		System.out.println("Arean är: " + area + "cm^2");
+		
+		long stortHeltal = 1000000000000000L;
+				
+		System.out.println(stortHeltal + 1);
+		
+		
+	}
+
+}
---
+++ VECKA37/VECKA37/ProvaSjalv.java
@@ -0,0 +1,18 @@
+package VECKA37;
+
+	public class ProvaSjalv {
+
+		public static void main(String[] args) {
+
+			double variable1 = 1; 
+	 
+			double variable2 = ((variable1*2)*variable1*variable1*variable1)*variable1*variable1*variable1;
+
+			double variable3 = (variable1*3)*variable1*variable1*variable1;
+			
+			System.out.println(variable2 - variable3);
+	
+	}
+	
+	
+}
