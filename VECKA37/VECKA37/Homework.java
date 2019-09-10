d4d0f0375ea8931d82e54187ccb2f42d27aafee8
Realgabbe <gabriel.billskog@elev.ga.ntig.se>
2019-09-10 12:17:04 +0200

    Update Homework.java

--- VECKA37/VECKA37/Homework.java
+++ VECKA37/VECKA37/Homework.java
@@ -1,10 +1,17 @@
 package VECKA37;
 
+import java.util.Scanner;
+
 public class Homework {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input  = new Scanner(System.in); // Behövs om man vill ha input
+		
+		
+		
+		
 	}
 
 }
