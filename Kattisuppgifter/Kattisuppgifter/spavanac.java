5b26ff77135d42de66f6a1527b3bb5afcb8a598f
Realgabbe <gabriel.billskog@elev.ga.ntig.se>
2019-10-06 14:32:26 +0200

    Update spavanac.java

--- Kattisuppgifter/Kattisuppgifter/spavanac.java
+++ Kattisuppgifter/Kattisuppgifter/spavanac.java
@@ -8,15 +8,26 @@ public class spavanac {
 		// TODO Auto-generated method stub
 
 		Scanner input = new Scanner(System.in);
-		
+
 		int H = 0;
 		int M = 0;
-		
+		int TMSum = 0;
+
 		H = input.nextInt();
 		M = input.nextInt();
-		
+
+		TMSum = H * 60 + M;
+		TMSum = TMSum - 45;
+
+		if (TMSum < 0) {
+			TMSum = TMSum + 60 * 24;
+		}
+
+		H = TMSum / 60;
+		M = TMSum - H * 60;
+
 		System.out.println(H + " " + M);
-		
+
 	}
 
 }
